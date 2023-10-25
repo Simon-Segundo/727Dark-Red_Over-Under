3467effa16582087303d4bfce7c44e9ea79e55e9
unknown <segundo.simon@tampaprep.org>
2023-10-25 16:58:19 -0400

    Commented all of the uncommented stuff and tried to fix terminator function but still a WIP

--- 727D_OverUnder/src/main.cpp
+++ 727D_OverUnder/src/main.cpp
@@ -1,24 +1,25 @@
-/*----------------------------------------------------------------------------*/
+/*-------------------------- 727D_OverUnder INFO -----------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
-/*    Author:       C:\Users\Engineering                                      */
+/*    Authors:      Alex, Elissa, Jack, Luca, Simon                           */
 /*    Created:      Wed Sep 13 2023                                           */
-/*    Description:  V5 project                                                */
+/*    Description:  727D Drive and Autonomous Code                            */
 /*                                                                            */
 /*----------------------------------------------------------------------------*/
 
-// ---- START VEXCODE CONFIGURED DEVICES ----
-// Robot Configuration:
-// [Name]               [Type]        [Port(s)]
-// frontLeft            motor         11              
-// midLeft              motor         13              
-// rearLeft             motor         12              
-// frontRight           motor         20              
-// midRight             motor         18              
-// rearRight            motor         19              
-// Controller1          controller                    
-// ---- END VEXCODE CONFIGURED DEVICES ----
+// -------- VEXCODE CONFIGURED DEVICES --------//
+// Robot Configuration:                        //
+// [Name]               [Type]        [Port(s)]//
+// frontLeft            motor         11       //     
+// midLeft              motor         13       //     
+// rearLeft             motor         12       //     
+// frontRight           motor         20       //     
+// midRight             motor         18       //     
+// rearRight            motor         19       //     
+// Controller1          controller             //     
+// --------------------------------------------//
 
+// Allows the library "vex.h" to be used
 #include "vex.h"
 
 using namespace vex;
@@ -27,7 +28,9 @@ using namespace vex;
 competition Competition;
 
 int numf = 0;
+int numc = 0;
 
+// Sets the velocity, torque, and the direction of the motor
 void set_spin(motor spin_motor, int Velocity, int Torque, bool fob) {
   spin_motor.setVelocity(Velocity, percent);
   spin_motor.setMaxTorque(Torque, percent);
@@ -38,6 +41,7 @@ void set_spin(motor spin_motor, int Velocity, int Torque, bool fob) {
   }
 }
 
+// Code for autonomous period to move the robot forward for a specific amount of time at a specific amount of velocity
 void forwardFor(int deg, int speed) {
   frontLeft.spinFor(deg, rotationUnits::deg, speed, velocityUnits::pct, false);
   midLeft.spinFor(deg, rotationUnits::deg, speed, velocityUnits::pct, false);
@@ -47,6 +51,7 @@ void forwardFor(int deg, int speed) {
   rearRight.spinFor(deg, rotationUnits::deg, speed, velocityUnits::pct, false);
 }
 
+// Code for autonomous period to move the robot in reverse for a specific amount of time at a specific amount of velocity
 void reverseFor(int deg, int speed) {
   frontLeft.spinFor(-deg, rotationUnits::deg, speed, velocityUnits::pct, false);
   midLeft.spinFor(-deg, rotationUnits::deg, speed, velocityUnits::pct, false);
@@ -56,8 +61,8 @@ void reverseFor(int deg, int speed) {
   rearRight.spinFor(-deg, rotationUnits::deg, speed, velocityUnits::pct, false);
 }
 
-void openClaw() {
-  int numc = 0;
+// Opens/Closes the claw on the front of the robot
+void clawCtrl() {
   if(numc == 0) {
     clawOpen.open();
     numc++;
@@ -67,6 +72,7 @@ void openClaw() {
   }
 }
 
+// Stops all motors
 void stopAllMotors() {
   frontLeft.stop();
   midLeft.stop();
@@ -76,17 +82,17 @@ void stopAllMotors() {
   rearRight.stop();
 }
 
+// Turns the robot 180 degrees
 void terminator() {
-  set_spin(frontLeft, 75, 100, true);
-  set_spin(midLeft, 75, 100, true);
-  set_spin(rearLeft, 75, 100, true);
-  set_spin(frontRight, 75, 100, false);
-  set_spin(midRight, 75, 100, false);
-  set_spin(rearRight, 75, 100, false);
-  wait(1, sec);
-  stopAllMotors();
+  frontLeft.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
+  midLeft.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
+  rearLeft.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
+  frontRight.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
+  midRight.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
+  rearRight.spinFor(3, timeUnits::sec, 75, velocityUnits::pct);
 }
 
+// Punches tri-ball forward when tri-ball is placed on top of robot
 void falconPunch() {
   if(numf == 0) {
     puncher.spin(fwd, 75, velocityUnits::pct);
@@ -97,10 +103,12 @@ void falconPunch() {
   }
 }
 
+// Code for autonomous period goes in this void
 void autonomous(void) {
 
 }
 
+// Code for drivercontrol period goes in this void
 void userControl(void) {
   while(1) {
     // Arcade steering using the left stick of the controller
@@ -110,24 +118,29 @@ void userControl(void) {
     frontRight.spin(fwd, (Controller1.Axis3.position(percent) - Controller1.Axis4.position(percent)), velocityUnits::pct);
     midRight.spin(fwd, (Controller1.Axis3.position(percent) - Controller1.Axis4.position(percent)), velocityUnits::pct);
     rearRight.spin(fwd, (Controller1.Axis3.position(percent) - Controller1.Axis4.position(percent)), velocityUnits::pct);
+
+    // Calls the openClaw function when button 'A' is pressed on the controller
+    Controller1.ButtonA.pressed(clawCtrl);
+
+    // Calls the terminator function when button 'R1' is pressed on the controller
+    Controller1.ButtonR1.pressed(terminator);
+
+    // Calls the falconPunch function when button 'B' is pressed on the controller
+    Controller1.ButtonB.pressed(falconPunch);
   }
 }
 
 int main() {
-  // Initializing Robot Configuration. DO NOT REMOVE!
+  // Initializing Robot Configuration.
   vexcodeInit();
-  
-  // Calls the openClaw function when button 'A' is pressed on the controller
-  Controller1.ButtonA.pressed(openClaw);
-  // Calls the terminator function when button 'R1' is pressed on the controller
-  Controller1.ButtonR1.pressed(terminator);
-  // Calls the falconPunch function when button 'B' is pressed on the controller
-  Controller1.ButtonB.pressed(falconPunch);
+
   // Sets the void autonomous to be run when autonomous mode begins
   Competition.autonomous(autonomous);
+
   // Sets the void userControl to be run when driver control mode begins
   Competition.drivercontrol(userControl);
 
+  // Keeps the code running forever until stopped
   while(true) {
     wait(100, msec);
   }
